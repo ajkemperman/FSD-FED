6c76df711fff035a439fcdc592c8995e6b164913
ajkemperman <ajkemperman@hetnet.nl>
2025-03-24 09:54:10 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react({ jsxImportSource: "react" })],
+  base: '/', // Ensures correct path handling
 
   test: {
     globals: true, // Enable global testing APIs like `vi` and `expect`
